e85228e8d2c84e95268853f6384e2e4621d66bba
Keezer83 <126595440+Keezer83@users.noreply.github.com>
2023-03-26 09:36:29 -0300

    first upload HTML CSS and start of js

---
+++ game.js
@@ -0,0 +1,4 @@
+const choices = ["rock", "paper", "scissors"];
+let playerScore = document.getElementById("pScore").innerHTML;
+let computerScore = document.getElementById("cScore").innerHTML;
+const playerChoices = document.querySelectorAll(".choices button");
