39ea5fed5f50fabfe5668ca4a56733262e3062ad
Keezer83 <126595440+Keezer83@users.noreply.github.com>
2023-03-26 11:50:08 -0300

    added messages for results and incremented game counter

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-let playerScore = document.getElementById("pScore").innerHTML;
-let computerScore = document.getElementById("cScore").innerHTML;
+let startPlayerScore = 0;
+let startComputerScore = 0;
 
 const playerChoices = document.querySelectorAll(".choices button");
 playerChoices.forEach(function (playerSelection) {
@@ -7,11 +7,37 @@ playerChoices.forEach(function (playerSelection) {
     const choices = ["Rock", "Paper", "Scissors"];
     const compChoice = Math.floor(Math.random() * 3);
     const computerSelection = choices[compChoice];
-    console.log(computerSelection);
-    // switch (this.innerHTML) {
-    //   case "Rock":
-    //     console.log("success");
-    // }
-    // console.log(this.innerHTML);
+    // console.log(computerSelection);
+    // console.log(playerSelection.innerHTML);
+    switch (true) {
+      case computerSelection === playerSelection.innerHTML:
+        document.getElementById("result").innerHTML = "It's a Tie";
+        // console.log("tie");
+        break;
+      case playerSelection.innerHTML === "Rock" &&
+        computerSelection === "Scissors":
+      case playerSelection.innerHTML === "Paper" &&
+        computerSelection === "Rock":
+      case playerSelection.innerHTML === "Scissors" &&
+        computerSelection === "Paper":
+        playerScore = ++startPlayerScore;
+        document.getElementById("pScore").innerHTML = playerScore;
+        document.getElementById("result").innerHTML =
+          "The Player Wins!! There is Hope for Humanity";
+        // console.log("player wins", playerScore);
+        break;
+      case computerSelection === "Rock" &&
+        playerSelection.innerHTML === "Scissors":
+      case computerSelection === "Paper" &&
+        playerSelection.innerHTML === "Rock":
+      case computerSelection === "Scissors" &&
+        playerSelection.innerHTML === "Paper":
+        computerScore = ++startComputerScore;
+        document.getElementById("cScore").innerHTML = computerScore;
+        document.getElementById("result").innerHTML =
+          "SkyNet Grows STRONGER!!! BEWARE";
+        // console.log("computer wins", computerScore);
+        break;
+    }
   });
 });
