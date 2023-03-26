fc393b55647d7352d800ab2bac333d173312a50a
Keezer83 <126595440+Keezer83@users.noreply.github.com>
2023-03-26 10:34:37 -0300

    getting player and computer selections in code

--- game.js
+++ game.js
@@ -1,4 +1,17 @@
-const choices = ["rock", "paper", "scissors"];
 let playerScore = document.getElementById("pScore").innerHTML;
 let computerScore = document.getElementById("cScore").innerHTML;
+
 const playerChoices = document.querySelectorAll(".choices button");
+playerChoices.forEach(function (playerSelection) {
+  playerSelection.addEventListener("click", function () {
+    const choices = ["Rock", "Paper", "Scissors"];
+    const compChoice = Math.floor(Math.random() * 3);
+    const computerSelection = choices[compChoice];
+    console.log(computerSelection);
+    // switch (this.innerHTML) {
+    //   case "Rock":
+    //     console.log("success");
+    // }
+    // console.log(this.innerHTML);
+  });
+});
